c5c9dd523ba4935ddb38c943daec8f28cfbb0a4d
rubbie kelvin <rubbiekelvinvoltsman@gmail.com>
2025-01-13 23:33:58 +0100

    chore: fixed type

--- src/pages/articles.vue
+++ src/pages/articles.vue
@@ -5,6 +5,6 @@ import DynLayout from "@/components/DynLayout.vue";
 <template>
   <DynLayout>
     <h1 class="lg:text-8xl md:text-7xl text-5xl">Articles</h1>
-    <p class="font-fira">I definitely have some articles coming up here. Follow my on twitter so you dont miss when that happens ;)</p>
+    <p class="font-fira">I definitely have some articles coming up here. Follow me on twitter so you dont miss when that happens ;)</p>
   </DynLayout>
 </template>
