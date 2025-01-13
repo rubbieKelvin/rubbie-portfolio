be7d73002bb8cf18f89e6643bb719b44ad34b241
rubbie kelvin <rubbiekelvinvoltsman@gmail.com>
2025-01-13 23:15:52 +0100

    chore: serve romans font directly

--- src/pages/faq.vue
+++ src/pages/faq.vue
@@ -7,22 +7,24 @@ import DynLayout from "@/components/DynLayout.vue";
     <h1 class="lg:text-8xl md:text-7xl text-5xl">Design choice</h1>
     <hr class="w-[40%] md:w-[20%]" />
     <p class="font-fira text-sm xl:max-w-[80%]">
-      This portfolio embraces a minimalistic, text-focused design inspired by
-      the timeless elegance of old typewritten newspapers and the clean
-      aesthetic of
-      <a href="https://p5aholic.me/" class="underline">Keita Yamada</a>'s work.
-      I wanted to create a space that prioritizes content over embellishments,
-      where every word and detail is intentional. The simplicity isn't just a
-      design choice, it reflects my belief in clarity, focus, and functionality.
-      It's also a nod to an era when design was as much about restraint as it
-      was about creativity. By stripping away excess, this portfolio serves as a
-      reminder that substance always outweighs style, and that good design
-      doesn't need to shout... it speaks clearly and confidently -
-      <a
-        href="https://youtu.be/6MCoJvAmnIY?si=dPbzirQ4cqHfRd11"
-        class="underline"
-        >https://youtu.be/6MCoJvAmnIY</a
-      >.
+      This portfolio uses a minimalistic, text-focused design inspired by old
+      typewritten newspapers and
+      <a href="https://p5aholic.me/" class="underline">Keita Yamada</a>'s clean
+      aesthetic. The goal is to keep things simple, clear, and focused on
+      content. I believe good design doesn't need to be flashy, it should be
+      functional, intentional, and easy to navigate. This approach reflects my
+      preference for substance over unnecessary style.
+      <br />
+      <br />
+      <span class="font-romans italic">
+        Checkout this&nbsp;
+        <a
+          href="https://youtu.be/6MCoJvAmnIY?si=dPbzirQ4cqHfRd11"
+          class="underline"
+          >boring video</a
+        >
+        &nbsp;on how i wrote this page.
+      </span>
     </p>
     <h2 class="md:text-3xl text-2xl mt-2">Copycats</h2>
     <hr class="w-[40%] md:w-[20%]" />
