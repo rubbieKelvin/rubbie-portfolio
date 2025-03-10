ec22c811dae25198a0228921632ee946f132ed7f
rubbie kelvin <rubbiekelvinvoltsman@gmail.com>
2025-03-10 18:24:26 +0100

    chore: updated resume

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -22,7 +22,7 @@ import DynLayout from "@/components/DynLayout.vue";
       >
     </p>
     <a
-      href="https://drive.google.com/file/d/1jQBVd9TMIipIlEZf4Gx45jkfR5qybBsH/view?usp=sharing"
+      href="https://drive.google.com/file/d/1OHI72FXBm46HS7XhxY_IObJM6buUcQ05/view?usp=sharing"
       target="_blank"
       class="font-fira mt-4 w-max dark:text-white"
     >
