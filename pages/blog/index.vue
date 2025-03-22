0c6296d02846216ab749312d4c9e2f1f5eb6c95d
rubbie kelvin <rubbiekelvinvoltsman@gmail.com>
2025-03-22 08:37:53 +0100

    chore: added rss link

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -15,13 +15,19 @@ const articles = computed(() => {
 
 <template>
   <div class="flex-grow flex gap-4 flex-col">
-    <h1 class="lg:text-8xl md:text-7xl text-5xl">Blog</h1>
+    <div class="flex items-center justify-between">
+      <h1 class="lg:text-8xl md:text-7xl text-5xl">Blog</h1>
+      <NuxtLink to="/rss.xml" external class="muted">rss</NuxtLink>
+    </div>
     <p v-if="articles.length === 0" class="xl:max-w-[60%]">
       I definitely have some articles coming up here. Follow my on x/twttr so
       you dont miss when that happens ;)
     </p>
     <div v-for="article in articles" :key="article.id">
-      <NuxtLink :to="article.path" class="flex gap-2 md:items-center md:flex-row flex-col">
+      <NuxtLink
+        :to="article.path"
+        class="flex gap-2 md:items-center md:flex-row flex-col"
+      >
         <div>
           <h2>~ {{ article.title }}</h2>
           <p class="text-sm italic mute">{{ article.description }}</p>
