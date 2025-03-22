f00294100437089f4a1a7f76e6091588b557ebae
rubbie kelvin <rubbiekelvinvoltsman@gmail.com>
2025-03-22 08:23:51 +0100

    chore: minor changes

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -17,19 +17,19 @@ const articles = computed(() => {
   <div class="flex-grow flex gap-4 flex-col">
     <h1 class="lg:text-8xl md:text-7xl text-5xl">Blog</h1>
     <p v-if="articles.length === 0" class="xl:max-w-[60%]">
-      I definitely have some articles coming up here. Follow my on twitter so
+      I definitely have some articles coming up here. Follow my on x/twttr so
       you dont miss when that happens ;)
     </p>
     <div v-for="article in articles" :key="article.id">
       <NuxtLink :to="article.path" class="flex gap-2 md:items-center md:flex-row flex-col">
         <div>
           <h2>~ {{ article.title }}</h2>
-          <p class="text-xs italic mute">{{ article.description }}</p>
+          <p class="text-sm italic mute">{{ article.description }}</p>
         </div>
 
         <div class="border-b border-dotted my-2 flex-grow hidden md:block" />
 
-        <p class="text-xs italic">{{ article.date }}</p>
+        <p class="text-sm italic">{{ article.date }}</p>
       </NuxtLink>
     </div>
   </div>
