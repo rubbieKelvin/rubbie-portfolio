627553433345d98c185013cc41ceb6ea0dc3ea53
rubbie kelvin <rubbiekelvinvoltsman@gmail.com>
2025-08-07 09:24:17 +0100

    chore: cleanup

--- content.config.ts
+++ content.config.ts
@@ -4,12 +4,19 @@ export default defineContentConfig({
   collections: {
     blog: defineCollection({
       type: "page",
-      source: "**/*.md",
+      source: "blog/*.md",
       schema: z.object({
         title: z.string(),
         tags: z.array(z.string()).optional(),
         description: z.string(),
         date: z.string(),
+      }),
+    }),
+    books: defineCollection({
+      type: "page",
+      source: "books/*.md",
+      schema: z.object({
+        title: z.string(),
       }),
     }),
   },
