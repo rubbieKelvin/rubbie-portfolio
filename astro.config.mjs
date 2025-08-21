be8963b73f1041fd8dc642268bd7b54f0fc196ae
rubbie kelvin <rubbiekelvinvoltsman@gmail.com>
2025-08-21 11:30:05 +0100

    chore: worked on layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,31 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+
+import sanity from "@sanity/astro";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()],
     resolve: {
       alias: {
-        '@': '/src',
-        '@/components': '/src/components',
-        '@/layouts': '/src/layouts',
-        '@/pages': '/src/pages',
-        '@/assets': '/src/assets'
-      }
-    }
-  }
+        "@": "/src",
+        "@/components": "/src/components",
+        "@/layouts": "/src/layouts",
+        "@/pages": "/src/pages",
+        "@/assets": "/src/assets",
+      },
+    },
+    plugins: [
+      // @ts-ignore
+      tailwindcss(),
+    ],
+  },
+  integrations: [
+    sanity({
+      projectId: "16odtdyl",
+      dataset: "production",
+      useCdn: false,
+    }),
+  ],
 });
