3059ef5028b53f45e9d30bfbdb215fbace393909
rubbie kelvin <rubbiekelvinvoltsman@gmail.com>
2025-08-21 11:10:59 +0100

    chore: base layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
+    plugins: [tailwindcss()],
     resolve: {
       alias: {
-        '~': '/src',
-        '~/components': '/src/components',
-        '~/layouts': '/src/layouts',
-        '~/pages': '/src/pages',
-        '~/assets': '/src/assets'
+        '@': '/src',
+        '@/components': '/src/components',
+        '@/layouts': '/src/layouts',
+        '@/pages': '/src/pages',
+        '@/assets': '/src/assets'
       }
     }
   }
