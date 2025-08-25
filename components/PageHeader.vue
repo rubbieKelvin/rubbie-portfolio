7f8a1fa2bc737c851dee47f487b008e5e800df64
rubbie kelvin <rubbiekelvinvoltsman@gmail.com>
2025-08-25 09:32:53 +0100

    chore: redirect

--- components/PageHeader.vue
+++ components/PageHeader.vue
@@ -4,12 +4,48 @@
       >rubbiekelvin@localhost ~ $ <span class="">status</span></NuxtLink
     >
     <div aria-hidden="true">
-      <span class="text-gray-400!">08-'25 - </span>
-      <span class="text-blue-300!">log</span>
-      <span class="text-yellow-300!"> [warning] </span>
-      <span class="text-gray-300!">
+      <span class="dark:text-gray-500!">11-08-25 - </span>
+      <span class="dark:text-yellow-500!"> [warning] </span>
+      <span class="">
         This site is under development & some parts are broken.
       </span>
     </div>
+    <div aria-hidden="true">
+      <span class="dark:text-gray-500!">25-08-25 - </span>
+      <span class="dark:text-blue-500!"> [info] </span>
+      <span class="">
+        This site is outdated and has moved to
+        <NuxtLink
+          to="www.rubbietheone.com"
+          external
+          class="underline text-blue-500!"
+          >www.rubbietheone.com</NuxtLink
+        >
+      </span>
+    </div>
+    <div aria-hidden="true">
+      <span class="dark:text-gray-500!">25-08-25 - </span>
+      <span class="dark:text-blue-500!"> [info] </span>
+      <span class=""> You'll be redirected in {{ timeLeft }} seconds </span>
+    </div>
   </div>
 </template>
+
+<script lang="ts" setup>
+const timeLeft = ref(5);
+let timeoutHandler: NodeJS.Timeout | null = null;
+
+onMounted(() => {
+  if (!window.origin.startsWith("http://localhost")) {
+    timeoutHandler = setInterval(() => {
+      if (timeLeft.value == 0) {
+        if (timeoutHandler) clearInterval(timeoutHandler);
+        window.location = "https://www.rubbietheone.com" as any;
+        return
+      }
+
+      timeLeft.value -= 1;
+    }, 1000);
+  }
+});
+</script>
