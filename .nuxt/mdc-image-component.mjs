310e085734b5ce307a77fd7a590bec42031d28f5
rubbie kelvin <rubbiekelvinvoltsman@gmail.com>
2025-08-25 17:42:20 +0100

    chore: removed nuxt based files

--- .nuxt/mdc-image-component.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "/Users/rubbiekelvin/Projects/rubbie/node_modules/@nuxt/image/dist/runtime/components/NuxtImg.vue"
--- .nuxt/ui/skeleton.ts
+++
@@ -1,3 +0,0 @@
-export default {
-  "base": "animate-pulse rounded-[calc(var(--ui-radius)*1.5)] bg-(--ui-bg-elevated)"
-}
