9c450897335d4e134268d43240c177a13b74d705
rubbie kelvin <rubbiekelvinvoltsman@gmail.com>
2025-09-13 20:51:38 +0100

    chore: changes to some rss

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,4 +1,4 @@
-import rss from "@astrojs/rss";
+import rss, { type RSSFeedItem } from "@astrojs/rss";
 import { sanityClient } from "sanity:client";
 import type { SanityDocument } from "@sanity/client";
 
@@ -13,7 +13,11 @@ export async function GET() {
       mainImage,
       publishedAt,
       external_link,
+      categories[] -> {
+        title
+      }
       "short_content": pt::text(body)
+      "content": pt::html(body)
     } {
       ...,
       "short_content": string::split(short_content, "")[0..40]
@@ -23,18 +27,39 @@ export async function GET() {
     }`
   );
 
+  let items = posts.map(
+    (post): RSSFeedItem => ({
+      title: post.title,
+      link: `${SITE_URL}/blog/${post.slug.current}`,
+      pubDate: post.publishedAt,
+      description: post.short_content,
+      content: post.content,
+      author: "dev.rubbie@gmail.com",
+      categories: post.categories.map(
+        (category: { title: string }) => category.title
+      ),
+      source: post.external_link
+        ? {
+            url: post.external_link,
+            title: post.title,
+          }
+        : undefined,
+    })
+  );
+
   return rss({
     title: "Rubbie kelvin",
     description:
       "Thoughts, tutorials, and insights from my development journey—sharing what I learnt along the way.",
     site: SITE_URL,
-    items: posts.map((post) => ({
-      title: post.title,
-      link: `${SITE_URL}/blog/${post.slug.current}`,
-      pubDate: post.publishedAt,
-      description: post.short_content,
-      content: post.short_content,
-    })),
-    customData: `<language>en-us</language>`,
+    items,
+    customData: `
+      <language>en-us</language>
+      <image>
+        <url>${SITE_URL}/rto.png</url>
+        <title>Rubbie Kelvin</title>
+        <link>${SITE_URL}</link>
+      </image>
+    `,
   });
 }
