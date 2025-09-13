e7e1a9c0f1fbde79f205371056bbd39b6d95c6dc
rubbie kelvin <rubbiekelvinvoltsman@gmail.com>
2025-09-13 21:47:37 +0100

    chore: fixed xml content

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,6 +1,8 @@
 import rss, { type RSSFeedItem } from "@astrojs/rss";
 import { sanityClient } from "sanity:client";
 import type { SanityDocument } from "@sanity/client";
+import { urlFor } from "@/lib/sanity";
+import { toHTML } from "@portabletext/to-html";
 
 const SITE_URL = "https://rubbietheone.com";
 
@@ -10,14 +12,14 @@ export async function GET() {
       _id,
       title,
       slug,
+      body,
       mainImage,
       publishedAt,
       external_link,
       categories[] -> {
         title
-      }
+      },
       "short_content": pt::text(body)
-      "content": pt::html(body)
     } {
       ...,
       "short_content": string::split(short_content, "")[0..40]
@@ -27,15 +29,35 @@ export async function GET() {
     }`
   );
 
-  let items = posts.map(
-    (post): RSSFeedItem => ({
+  let items = posts.map((post): RSSFeedItem => {
+    const mainImage = post.mainImage ? urlFor(post.mainImage).url() : null;
+    let content: string = toHTML(post.body, {
+      components: {
+        types: {
+          code(props) {
+            // const language = props.value?.language ?? "text";
+            return `<pre><code>${props.value?.code}</code></pre>`;
+          },
+          image(props) {
+            const imageUrl = post.mainImage ? urlFor(props.value).url() : null;
+            return `<img src="${imageUrl}" alt="${props.value?.altText || "Image"}" />`;
+          },
+        },
+      },
+    });
+
+    if (mainImage) {
+      content = `<img src="${mainImage}" alt="${post.title}" />\n\n${content}`;
+    }
+
+    return {
       title: post.title,
       link: `${SITE_URL}/blog/${post.slug.current}`,
       pubDate: post.publishedAt,
       description: post.short_content,
-      content: post.content,
+      content,
       author: "dev.rubbie@gmail.com",
-      categories: post.categories.map(
+      categories: post.categories?.map(
         (category: { title: string }) => category.title
       ),
       source: post.external_link
@@ -44,8 +66,8 @@ export async function GET() {
             title: post.title,
           }
         : undefined,
-    })
-  );
+    };
+  });
 
   return rss({
     title: "Rubbie kelvin",
@@ -59,6 +81,9 @@ export async function GET() {
         <url>${SITE_URL}/rto.png</url>
         <title>Rubbie Kelvin</title>
         <link>${SITE_URL}</link>
+        <width>144</width>
+        <height>400</height>
+        <description>Rubbie Kelvin's blog</description>
       </image>
     `,
   });
